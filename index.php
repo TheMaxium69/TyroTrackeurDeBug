671f1a93759297158b3de18093f5c02929a82016
TheMaxium69 <63310746+TheMaxium69@users.noreply.github.com>
2021-06-01 10:04:10 +0200

    start disgn

--- index.php
+++ index.php
@@ -1,1 +1,54 @@
-<?php
+<!doctype html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <title>TyroServ BugTrackeur</title>
+    <link href="http://tyrolium.fr/Contenu/Image/Tyrolium ServerMC Sword" rel="shortcut icon">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" media="screen" href="styles.css">
+</head>
+<body>
+    <header>
+        <nav class="navbar navbar-light bg-light">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">
+                    <img src="http://tyrolium.fr/Contenu/Image/Tyrolium ServerMC Sword 3D" alt="" width="30" height="30" class="d-inline-block align-text-top">
+                    TyroServ BugTracker
+                </a>
+                <form class="d-flex">
+                    <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                    <button class="btn btn-outline-dark" type="submit">Search</button>
+                </form>
+            </div>
+        </nav>
+    </header>
+    <main>
+        <section id="head">
+            <div style="padding: 40px">
+                <H2>Bienvenue dans le trackeur de bug de TyroServ</H2>
+            </div>
+        </section>
+
+        <section id="list">
+            <div class="list-group">
+                <button type="button" class="list-group-item list-group-item-action active" aria-current="true">First</button>
+                <button type="button" class="list-group-item list-group-item-action">Second</button>
+                <button type="button" class="list-group-item list-group-item-action">ss</button>
+                <button type="button" class="list-group-item list-group-item-action">A fourth button item</button>
+                <button type="button" class="list-group-item list-group-item-action" disabled>A disabled button item</button>
+            </div>
+        </section>
+
+
+
+
+    </main>
+
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+</html>
+
+
